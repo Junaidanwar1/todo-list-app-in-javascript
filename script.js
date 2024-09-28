846835fcf53ea65545cec8b09c0c5394bd643cf5
Junaidanwar1 <anwarjunaid853@gmail.com>
2024-09-28 09:39:13 +0500

    adding function to js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function addtask(){
     var tasklist = document.getElementById('tasklist');
 
     if(!taskinput.value.trim()){
-        return alert("todo can't empty...")
+        return alert("todo can not be empty...")
     }
 
     var li = document.createElement('li');
