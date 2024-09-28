f0050f4614f7d53ec99083b1529f1e406a210f4a
Junaidanwar1 <anwarjunaid853@gmail.com>
2024-09-28 09:33:59 +0500

    adding javascript functionality

---
+++ script.js
@@ -0,0 +1,26 @@
+function addtask(){
+    var taskinput = document.getElementById('taskinput');
+    var tasklist = document.getElementById('tasklist');
+
+    if(!taskinput.value.trim()){
+        return alert("todo can't empty...")
+    }
+
+    var li = document.createElement('li');
+    li.textContent = taskinput.value;
+    tasklist.appendChild(li);
+
+    var removeButton = document.createElement('button');
+    removeButton.textContent='Remove';
+    li.appendChild(removeButton);
+
+
+    removeButton.className='remove-btn';
+    removeButton.onclick = function(){
+        tasklist.removeChild(li);
+    };
+    li.onclick=function(){
+        li.classList.toggle('completed');
+    };
+    taskinput.value='';
+}
